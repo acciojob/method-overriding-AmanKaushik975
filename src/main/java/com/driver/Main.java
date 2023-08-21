ce9fa16d7023963881eb7d4b3af7300ad6f7a8a6
amank <aman.kaushik.975@gmail.com>
2023-08-21 12:38:37 +0530

    try1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
+class A {
+    String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    @Override
+    String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+
 public class Main {
+    public static void main(String[] args) {
+        B objB = new B();
+        System.out.println(objB.meth());
+    }
   
 }
