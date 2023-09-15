b4f56e7fc90e5e90372177bfa628dcf493edbfff
amank <aman.kaushik.975@gmail.com>
2023-09-15 18:51:07 +0530

    submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,7 +15,7 @@ class B extends A {
     }
 }
 
-public class Main {
+public class Main { 
     public static void main(String[] args) {
         B objB = new B();
         System.out.println(objB.meth());
